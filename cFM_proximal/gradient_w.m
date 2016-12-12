4899c7610ee92806f9df855bc155d19ccd840f85
needonature <244496603@qq.com>
2016-12-12 14:52:25 -0500

    Update gradient_w.m

--- cFM_proximal/gradient_w.m
+++ cFM_proximal/gradient_w.m
@@ -2,7 +2,7 @@ function w=gradient_w(y,X,w,Z,alpha)
 	alpha0=0.5;
 	beta0=0.1;
 	epsilon=1e-5;
-	f_pre=f1(y,X,w,Z,alpha)
+	f_pre=f1(y,X,w,Z,alpha);
 	while true
 		t=1;
 		grad_f1_=grad_f1(y,X,w,Z,alpha);
@@ -15,6 +15,6 @@ function w=gradient_w(y,X,w,Z,alpha)
 	    if abs(f_pre-f1(y,X,w,Z,alpha))<epsilon
 	        break
 	    end
-	    f_pre=f1(y,X,w,Z,alpha)
+	    f_pre=f1(y,X,w,Z,alpha);
    	end
 end
